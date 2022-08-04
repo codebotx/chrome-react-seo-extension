a297bac31363f2b503f0e80bc1ee7c2a0b35cee5
anubhab-patnaik <anubhab.patnaik@squbix.com>
2022-08-04 15:25:31 +0530

    fixed bug: run ayrn craco build not yarn build

--- src/App.tsx
+++ src/App.tsx
@@ -29,47 +29,16 @@ function App() {
 
         { type: 'GET_DOM' } as DOMMessage,
 
+
         (response: DOMMessageResponse) => {
           setTitle(response.title);
           setHeadlines(response.headlines);
         },
-
-
       );
+      // console.log({ type: 'GET_DOM' } as DOMMessage);
+
     });
   }, []);
-
-
-
-
-
-
-
-
-
-
-
-
-  //   /**
-  //    * We can't use "chrome.runtime.sendMessage" for sending messages from React.
-  //    * For sending messages from React we need to specify which tab to send it to.
-  //    */
-
-  //   chrome.tabs && chrome.tabs.query({
-  //     active: true,
-  //     currentWindow: true
-  //   }, tabs => {
-  //     console.log(tabs[0].id);
-  //     /**
-  //      * Sends a single message to the content script(s) in the specified tab,
-  //      * with an optional callback to run when a response is sent back.
-  //      *
-  //      * The runtime.onMessage event is fired in each content script running
-  //      * in the specified tab for the current extension.
-  //  */
-  // });
-  // }, []);
-
 
   return (
     <div className="App">
